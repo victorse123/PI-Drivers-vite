5c62e6ea0f97b9e2bfbcb2684cb868e2a430e137
Victor Sepulveda <victorseva123@gmail.com>
2023-12-25 11:45:45 -0500

    Actual 25/12/23

--- server/src/controllers/teamController.js
+++ server/src/controllers/teamController.js
@@ -38,3 +38,12 @@ const getAllTeams = async () => {
 };
 
 module.exports = { getAllTeams };
+
+
+//Este controlador se encarga de obtener todos los equipos de la base de datos local y, en caso de que no haya equipos, los recopila de la API externa y los guarda en la base de datos local. Veamos algunos puntos relevantes:
+
+//Método getAllTeams:
+//Verifica si hay equipos en la base de datos local.
+//Si no hay equipos, realiza una solicitud a la API externa para obtener datos de conductores.
+//Filtra y procesa la información obtenida de la API, extrayendo los equipos de los conductores.
+//Elimina duplicados y crea instancias de Team en la base de datos local.
