34372bb5bb8fc3e172cfcfe0d7e050b956996b51
Victor Sepulveda <victorseva123@gmail.com>
2023-12-30 17:13:05 -0500

    Actual 30/12/23

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+/* eslint-disable react/no-deprecated */
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import React from 'react'
 import './index.css'
+import {BrowserRouter} from "react-router-dom"
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+    <BrowserRouter>
+      <App/>
+      </BrowserRouter>
+    </Provider>
+      </React.StrictMode>, 
+  document.getElementById("root")
 )
