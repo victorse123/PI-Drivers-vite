7a4a70a749abebc55c96961bb9987e8435f50ef6
Victor Sepulveda <victorseva123@gmail.com>
2023-12-30 17:08:52 -0500

    Actual 30/12/23

---
+++ client/src/redux/Store.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducer";
+import thunk from "redux-thunk";
+
+// la extensión de Redux DevTools solo se active en el entorno de desarrollo y no en producción
+const composeEnhancers =
+  (process.env.NODE_ENV === "development" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
