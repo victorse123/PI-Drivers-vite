3bbafed059568efbf4cb50196c61157b11a2585b
Victor Sepulveda <victorseva123@gmail.com>
2023-12-30 17:10:25 -0500

    Actual 30/12/23

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import  React from "react";
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from "./components/LandingPage/LandingPage"
+import FormPage from "./components/FormPage/FormPage"
+import Home from "./components/Home/Home"
+import Detail from './components/Detail/Detail';
+import Error from "./components/Error/Error";
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() { 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+ <Routes>
+        <Route exact path="/" element={<LandingPage/>} />
+        <Route  path='/home' element={<Home/>}/>
+        <Route  path='/detail/:id' element={<Detail/>}/>
+        
+        <Route  path="/create" element={<FormPage/>}/>
+        <Route path="*" element={<Error />} />
+</Routes>
+    </div>
+  );
 }
+
 
 export default App
