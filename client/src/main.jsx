37bd38ce289a9eafbad43b3059429f6f8e517af4
Victor Sepulveda <victorseva123@gmail.com>
2024-01-09 21:17:39 -0500

    Actual 09/01/24

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,41 @@
-/* eslint-disable react/no-deprecated */
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import React from 'react'
-import './index.css'
-import {BrowserRouter} from "react-router-dom"
-import { Provider } from 'react-redux'
-import store from './redux/store'
+// /* eslint-disable react/no-deprecated */
+// import ReactDOM from 'react-dom'
+// import App from './App.jsx'
+// import React from 'react'
+// import './index.css'
+// import {BrowserRouter} from "react-router-dom"
+// import { Provider } from 'react-redux'
+// import store from './redux/store'
 
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//     <BrowserRouter>
+//       <App/>
+//       </BrowserRouter>
+//     </Provider>
+//       </React.StrictMode>, 
+//   document.getElementById("root")
+// )
+
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import React from 'react';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+const root = document.getElementById('root');
+const rootContainer = createRoot(root);
+
+rootContainer.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
-      <App/>
+      <BrowserRouter>
+        <App />
       </BrowserRouter>
     </Provider>
-      </React.StrictMode>, 
-  document.getElementById("root")
-)
+  </React.StrictMode>
+);
